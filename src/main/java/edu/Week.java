c5a7c3f064f9d8dad6ba4005c1c56e2350803b4e
garenigho <garenigho8@gmail.com>
2025-03-26 10:47:35 -0400

    Wrote methods for User and Week

--- src/main/java/edu/Week.java
+++ src/main/java/edu/Week.java
@@ -1,5 +1,13 @@
 package edu;
+import java.util.ArrayList;
+
+
+import javax.management.RuntimeErrorException;
 
 public class Week {
-    
+    private ArrayList<Choices> choices;
+
+    public Week(){
+        throw new IllegalArgumentException("Not implemented yet");
+    }
 }
