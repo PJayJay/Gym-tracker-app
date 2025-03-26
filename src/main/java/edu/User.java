1db1b17b4d7ddb5b7ad57e3304e3f4b318e37637
garenigho <garenigho8@gmail.com>
2025-03-26 10:47:35 -0400

    Wrote methods for User and Week

--- src/main/java/edu/User.java
+++ src/main/java/edu/User.java
@@ -1,5 +1,23 @@
 package edu;
 
+import java.util.ArrayList;
+
 public class User {
-    
+    private int height;
+    private int weight;
+    public int avail;
+    public Restriction limits;
+    public ArrayList<String> goals;
+
+    public void changeWeight(int newWeight) {
+        weight = newWeight;
+    }
+
+    public void changeHeight(int newHeight) {
+        height = newHeight;
+    }
+
+    public void updateAvailability(int newAvail) {
+        avail = newAvail;
+    }
 }
